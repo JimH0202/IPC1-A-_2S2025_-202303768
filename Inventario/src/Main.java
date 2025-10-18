33de07aff293c6203785efa77ce9efa97af4d022
JimH0202 <odemhurtarte@gmail.com>
2025-10-18 00:06:29 -0600

    [commit proyecto 2]

--- Inventario/src/Main.java
+++ Inventario/src/Main.java
@@ -2,10 +2,11 @@ import java.util.Scanner;
 
 public class Main {
     private static String usuarioActual; // usuario para acción
+    private static String sucursal; 
 
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
-            // 🔹 pedir usuario al iniciar
+            //  pedir usuario al iniciar
             System.out.print("Ingrese su nombre de usuario: ");
             usuarioActual = scanner.nextLine();
 
@@ -32,6 +33,7 @@ public class Main {
                     case 6 -> Reportes.bitacora();
                     case 7 -> mostrarDatosEstudiante();
                     case 8 -> System.out.println("Saliendo del sistema...");
+                    case 9 -> sucursal();
                     default -> System.out.println("Opción Incorrecta. Intente de nuevo.");
                 }
 
@@ -50,6 +52,7 @@ public class Main {
         System.out.println("6. Bitácora");
         System.out.println("7. Ver datos del estudiante");
         System.out.println("8. Salir");
+        system.out.println(x:"9. crear sucursal");
         System.out.println("------------------------------------------------");
     }
 
@@ -60,4 +63,16 @@ public class Main {
         System.out.println("Curso: Laboratorio de Introducción a la Programación y Computación 1");
         System.out.println("Sección: A");
     }
+
+
+
+    private static void sucursal () {
+            try (Scanner scanner = new Scanner(System.in)) {
+                //  pedir usuario al iniciar
+                System.out.print("Ingrese su nombre de usuario: ");
+                sucursal = scanner.nextLine();
+            }
+
+        }
+
 }
